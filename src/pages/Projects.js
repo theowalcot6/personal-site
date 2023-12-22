269ae3f73e60b85bad6f488a9dcdcfac022b0200
Theo Walcot <101593502+theowalcot6@users.noreply.github.com>
2023-12-22 15:58:55 +0000

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,8 +15,8 @@ const Projects = () => {
                 <h6 className='Projects-header'>My Projects</h6>
                 <div className='ProjectCard-container'>
                     <ProjectCard title='Stuft' text='Classical Education is inefficient. Studying becomes difficult and time-pressured. Stuft maximises your chance of A-A* grades in the smallest amount of time.' link='https://stuft.co.uk/' backgroundImg={Stuft} roles={['Founder', 'Software Developer']} />
-                    <ProjectCard title='Looby' text='A Subscription as a Service that turns your Asana tickets into a Product roadmap that can be shared with key stakeholders in the customers business.' link='https://looby.co.uk/' backgroundImg={Looby} roles={['Founder', 'Software Developer']}/>
-                    <ProjectCard title='Codeingo' text='The coding equivalent of Duolingo. Learn to code in a gamified setting with leaderboards and exciting challenges.' link='https://codeingo.co.uk/' backgroundImg={Codeingo} roles={['Founder', 'Software Developer']}/>
+                    <ProjectCard title='Looby' text='A Subscription as a Service that turns your Asana tickets into a Product roadmap that can be shared with key stakeholders in the customers business.' link='https://luuby.co.uk/' backgroundImg={Looby} roles={['Founder', 'Software Developer']}/>
+                    <ProjectCard title='Quack Marketing' text='A digital agency that I work at as the Head of Development focussing on Bespoke Software Development and Digital Strategy' link='https://quackmarketing.co.uk/' backgroundImg={Codeingo} roles={['Founder', 'Software Developer']}/>
                 </div>
             </div>
             <Footer styling={'free'} />
